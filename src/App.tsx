d9e6dcbe846a2ff771d710e7780e0344b5fffa46
Ayush <ayushwilltry@gmail.com>
2025-01-26 17:18:12 +0545

    my commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import './App.css'
+import Cake from './components/Cake'
+
+// import SarojSir from '/saroj.jfif'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div classNameName=''>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+        <bokeh></bokeh>
+      <div className="pyro">
+        <div className="before"></div>
+        <div className="after"></div>
+      </div>
+      <div style={{ marginInline: 'auto' }} className='confetti-container'>
+        <h1>{`<BlazeCodes />`}</h1>
+        <div className='img-holder'>
+          <img src="/public/srzsir.jfif" alt="Saroj"  className='image-image'/>
+        </div>
+        <h2>Happy Birthday Saroj Sir!</h2>
+        <span className='conffeti-holder'>🎉</span>
+      </div>
+      <Cake />
+
+    </div>
+  )
+}
+
+export default App
